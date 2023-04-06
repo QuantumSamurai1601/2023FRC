5bfdcfbaf736c81d552f3b182d0597f77dbcea06
quinzo <dabeastking347@gmail.com>
2023-04-06 15:31:49 -0400

    Added extension constraints

--- src/main/java/frc/robot/subsystems/Arm/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/Arm/ArmSubsystem.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems.Arm;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
