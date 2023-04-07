ed34fcbef481bde86434e6f078d865be39e306c4
quinzo <dabeastking347@gmail.com>
2023-04-07 14:57:52 -0400

    Balance auton

--- src/main/java/frc/robot/Commands/Autons/balanceAuton.java
+++ src/main/java/frc/robot/Commands/Autons/balanceAuton.java
@@ -1,11 +1,12 @@
 package frc.robot.Commands.Autons;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Commands.Drive.balance;
 import frc.robot.Commands.Drive.drive;
 import frc.robot.subsystems.Drive.DriveSubsystem;
 
-public class balance extends SequentialCommandGroup{
-    public balance(DriveSubsystem driveSubsystem){
+public class balanceAuton extends SequentialCommandGroup{
+    public balanceAuton(DriveSubsystem driveSubsystem){
         addRequirements(driveSubsystem);
         addCommands(
             new drive(driveSubsystem, 1, 0, 0, true, false , 1.5)
